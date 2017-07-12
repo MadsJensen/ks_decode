306237f9089b9447d0088070522396f47ba631a9
Mads Jensen <mje.mads@gmail.com>
2017-07-12 16:32:57 +0200

    Initial commit

---
+++ my_settings.py
@@ -0,0 +1,9 @@
+
+
+raw_path = "/Volumes/Data/kristian_project/tilMads/"
+data_path = "/Volumes/Data/kristian_project/"
+
+epochs_folder = data_path + "epochs_folder/"
+mvpa_folder = data_path + "mvpa_folder/"
+
+
