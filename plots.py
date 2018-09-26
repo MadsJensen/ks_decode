bc71fdaed427c5357ee8eef2661349c5062cdd68
Mads Jensen <mje.mads@gmail.com>
2018-09-26 10:48:49 +0200

    FIX: clean up of code, and remove means

--- plots.py
+++ plots.py
@@ -13,9 +13,10 @@ times = np.linspace(-0.1, 1.435, 308)
 for condition in conditions:
     for subject in subjects:
         if condition is 'all':
-            scores = np.load('%s_time_gen_score.npy' % subject)
+            scores = np.load('%s_time_gen_score_svm.npy' % subject)
         else:
-            scores = np.load('%s_time_gen_score_%s.npy' % (subject, condition))
+            scores = np.load('%s_time_gen_score_%s_svm.npy' % (subject,
+                                                             condition))
 
         scores = np.mean(scores, axis=0)
 
@@ -28,7 +29,7 @@ for condition in conditions:
         ax.legend()
         ax.axvline(.0, color='k', linestyle='-')
         ax.set_title('Decoding MEG sensors over time')
-        plt.savefig('%s_%s_diag.png' % (subject, condition))
+        plt.savefig('plots/%s_%s_diag.png' % (subject, condition))
 
         # Plot the full matrix
         fig, ax = plt.subplots(1, 1)
@@ -46,7 +47,7 @@ for condition in conditions:
         ax.axvline(0, color='k')
         ax.axhline(0, color='k')
         plt.colorbar(im, ax=ax)
-        plt.savefig('%s_%s_TG.png' % (subject, condition))
+        plt.savefig('plots/%s_%s_TG.png' % (subject, condition))
 
 for condition in conditions:
     scores_all = np.empty((len(subjects), 308, 308))
@@ -59,7 +60,6 @@ for condition in conditions:
             scores_all[jj] = np.load(
                 '%s_time_gen_score_%s.npy' % (subject, condition)).mean(axis=0)
 
-    tstats_diag, pvals_diag = stats.ttest_1samp(np.diag(scores_all), 0.5)
     tstat_tg, pvals_tg = stats.ttest_1samp(scores_all, 0.5)
     rejectd_tg, pvals_tg_fdr = fdr_correction(pvals_tg)
     rejectd_diag, pvals_diag_fdr = fdr_correction(np.diag(pvals_tg))
@@ -68,31 +68,30 @@ for condition in conditions:
     scores_all = scores_all.mean(axis=0)
     # Plot the diagonal (it's exactly the same as the time-by-time decoding above)
     fig, ax = plt.subplots()
-    ax.plot(times, np.diag(scores_all.mean(axis=0)), label='score')
+    ax.plot(times, np.diag(scores_all), label='score')
     ax.axhline(.5, color='k', linestyle='--', label='chance')
     ax.set_xlabel('Times')
     ax.set_ylabel('AUC')
     ax.legend()
     ax.axvline(.0, color='k', linestyle='-')
     ax.set_title('Decoding MEG sensors over time')
-    plt.savefig('all_%s_diag.png' % condition)
+    plt.savefig('plots/all_%s_diag_equal.png' % condition)
 
-    fig, ax = plt.subplots()
-    ax.plot(times, np.diag(tstat_tg), label='score')
-    ax.axhline(.5, color='k', linestyle='--', label='chance')
-    ax.axhline(threshold_fdr, color='k',  linestyle='--', label='fdr threshold')
-    ax.set_xlabel('Times')
-    ax.set_ylabel('AUC')
-    ax.legend()
-    ax.axvline(.0, color='k', linestyle='-')
-    ax.set_title('Decoding MEG sensors over time')
-    plt.show()
+    # fig, ax = plt.subplots()
+    # ax.plot(times, np.diag(tstat_tg), label='score')
+    # ax.axhline(.5, color='k', linestyle='--', label='chance')
+    # ax.axhline(threshold_fdr, color='k',  linestyle='--', label='fdr threshold')
+    # ax.set_xlabel('Times')
+    # ax.set_ylabel('AUC')
+    # ax.legend()
+    # ax.axvline(.0, color='k', linestyle='-')
+    # ax.set_title('Decoding MEG sensors over time')
     # plt.savefig('all_%s_diag.png' % condition)
 
     # Plot the full matrix
     fig, ax = plt.subplots(1, 1)
     im = ax.imshow(
-        scores_all.mean(axis=0),
+        scores_all,
         interpolation='lanczos',
         origin='lower',
         cmap='RdBu_r',
@@ -105,4 +104,4 @@ for condition in conditions:
     ax.axvline(0, color='k')
     ax.axhline(0, color='k')
     plt.colorbar(im, ax=ax)
-    plt.savefig('all_%s_TG.png' % condition)
+    plt.savefig('plots/all_%s_TG_equal.png' % condition)
